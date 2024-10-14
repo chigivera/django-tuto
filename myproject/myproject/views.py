fa03cb3794ac43d9c0529891ac08d16aec8f0488
chmakha2002 <chmakha2002@gmail.com>
2024-10-14 12:50:18 +0000

    lesson 1

---
+++ myproject/myproject/views.py
@@ -0,0 +1,9 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+def home_page(request):
+    # return HttpResponse("Hello World! I'm Home")
+    return render(request,'home.html')
+
+def about_page(request):
+    # return HttpResponse("About Page!")
+    return render(request,'about.html')
---
+++ myproject/static/js/main.js
@@ -0,0 +1,1 @@
+console.log("this is js from ur about page")
